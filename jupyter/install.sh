943ab72b77e3682941bec832406b0aa0cf970742
Hikmat Dhamee <me.hemant.available@gmail.com>
2024-11-27 23:13:25 +0545

    adding projects

---
+++ jupyter/install.sh
@@ -0,0 +1,15 @@
+# it is a classic interactive python web based IDE
+pip install notebook
+
+# Launch notebook: After installation, you can start notebook by running:
+jupyter notebook
+
+# JupyterLab is a more advanced interface for Jupyter that provides more functionality
+# (such as file management, markdown previews, integrated terminals, etc.). You can install JupyterLab if you'd like:
+pip install jupyterlab
+
+# Launch JupyterLab: After installation, you can start JupyterLab by running:
+jupyter lab
+
+# your browser should automatically open a tab with the Jupyter interface, which usually runs on http://localhost:8888.
+
